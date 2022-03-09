b701e95e1bcbd6fdd78d3d8556ac8776de51c1a2
tomas <tcerdeira@itba.edu.ar>
2022-03-09 17:28:55 -0300

    Added basic json input file to read initial state from

--- TP1/main.py
+++ TP1/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 from TP1.data_structs.EightState import EightState
 from TP1.algos.not_informed import dfs
@@ -7,7 +8,11 @@ from TP1.utils.heuristics import basic_heuristic
 
 
 def main():
-    matrix = [[6, 8, 4], [3, 5, 7], [0, 1, 2]]
+    input_file = open('input.json')
+    data = json.load(input_file)
+    matrix_from_json = [data['start_state']['0'], data['start_state']['1'], data['start_state']['2']]
+    # matrix = [[6, 8, 4], [3, 5, 7], [0, 1, 2]]
+    matrix = matrix_from_json
     if not EightState.is_matrix_solvable(matrix):
         print("This matrix does not correspond to a valid state in the game")
         return
