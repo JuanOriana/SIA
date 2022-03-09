77d2d02d3826620851e62c6ada7c86f49be5fead
Juan <joriana@itba.edu.ar>
2022-03-09 17:08:51 -0300

    Added requirements

--- TP1/main.py
+++ TP1/main.py
@@ -1,18 +1,18 @@
 import numpy as np
 
 from TP1.data_structs.EightState import EightState
-from TP1.algos.not_informed import bfs
+from TP1.algos.not_informed import dfs
 from TP1.algos.informed import hill_climbing_global
 from TP1.utils.heuristics import basic_heuristic
 
 
 def main():
-    matrix = [[7, 1, 4], [8, 5, 6], [0, 2, 3]]
+    matrix = [[6, 8, 4], [3, 5, 7], [0, 1, 2]]
     if not EightState.is_matrix_solvable(matrix):
         print("This matrix does not correspond to a valid state in the game")
         return
     board = EightState(np.matrix(matrix, dtype=int))
-    bfs(board)
+    dfs(board)
 
 
 if __name__ == "__main__":
