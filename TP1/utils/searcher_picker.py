517053ea0a93b569b13e1f31bb24b174b15f3130
santigarciam <sangarcia@itba.edu.ar>
2022-03-11 20:44:40 -0300

    Added fat heuristic and bppv algorithm to json parser

--- TP1/utils/searcher_picker.py
+++ TP1/utils/searcher_picker.py
@@ -2,13 +2,14 @@ from TP1.searchers.informed.LocalHeuristicSearcher import LocalHeuristicSearcher
 from TP1.searchers.informed.ponderated.AStarSearcher import AStarSearcher
 from TP1.searchers.informed.ponderated.GlobalHeuristicSearcher import GlobalHeuristicSearcher
 from TP1.searchers.uninformed.BFSSearcher import BFSSearcher
+from TP1.searchers.uninformed.DFSIterativeSearcher import DFSIterativeSearcher
 from TP1.searchers.uninformed.DFSSearcher import DFSSearcher
-from TP1.utils.heuristics import deep_heuristic, basic_heuristic
+from TP1.utils.heuristics import deep_heuristic, basic_heuristic, fat_heuristic
 
 informed_algorithms = {'a_star': AStarSearcher, 'local_heuristic': LocalHeuristicSearcher,
                        'global_heuristic': GlobalHeuristicSearcher}
-not_informed_algorithms = {'bpa': DFSSearcher, 'bpp': BFSSearcher}
-heuristics_functions = {'basic': basic_heuristic, 'deep': deep_heuristic}
+not_informed_algorithms = {'bpa': DFSSearcher, 'bpp': BFSSearcher, 'bppv': DFSIterativeSearcher}
+heuristics_functions = {'basic': basic_heuristic, 'deep': deep_heuristic, 'fat': fat_heuristic}
 
 
 def searcher_picker(algo_name, heuristic):
