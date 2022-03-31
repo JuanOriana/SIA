d764c9bdd46ca547b5cd5b3d5991915565fad4da
JuanOriana <joriana@itba.edu.ar>
2022-03-30 21:56:32 -0300

    Added new test cases for longer runtimes

--- TP2/utils/aptitude.py
+++ TP2/utils/aptitude.py
@@ -2,8 +2,17 @@ from TP2.data_structs.Individual import Individual
 from TP2.data_structs.TestCase import TestCase
 from TP2.utils.logistic_function import logistic_function
 
-test_cases = [TestCase(1, [-3.9429, -0.7689, 4.8830]), TestCase(1, [4.1793, -4.9218, 1.7664]),
-              TestCase(0, [4.4793, -4.0765, -4.0765])]
+test_cases = [TestCase(1, [-3.9429, -0.7689, 4.8830]),
+              TestCase(1, [4.1793, -4.9218, 1.7664]),
+              TestCase(0, [4.4793, -4.0765, -4.0765]),
+              #ARTIFICIAL CASES BASED ON load_data.py
+              TestCase(0.9999999960102027, [-3.10572066, -1.22058696,  1.18943874]),
+              TestCase(1.2483102208380704e-08, [2.36450485, -0.22315417, -3.49505961]),
+              TestCase(0.9999999963441942, [-1.59391869, -1.08726703, 3.47960297]),
+              TestCase(1.2483100108323224e-08, [-0.07845372,  3.58493092, -3.5772667 ]),
+              TestCase(0.999999996342801, [0.91036564, 2.70380505, 1.51847133]),
+              TestCase(0.9999999963441943, [ 3.40277313, -2.74126483,  3.86870491]),
+              ]
 
 def big_f(indiv: Individual, case: TestCase):
     x = 0
