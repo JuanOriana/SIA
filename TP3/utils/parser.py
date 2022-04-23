14aef2a2b8121a183a2a2ed13417ea241dc3664d
JuanOriana <joriana@itba.edu.ar>
2022-04-23 11:07:52 -0300

    Numbers parsing

--- TP3/utils/parser.py
+++ TP3/utils/parser.py
@@ -1,6 +1,23 @@
+import numpy as np
 import pandas as pd
 
 
 def parse(file_name: str):
-    df = pd.read_csv(file_name, sep=' +', engine='python')
+    df = pd.read_csv(file_name, sep=' +', engine='python', header=None)
     return df.to_numpy()
+
+
+def parse_nums(file_name: str, height: int):
+    df = pd.read_csv(file_name, sep=' +', engine='python', header=None)
+    arrayed = df.to_numpy()
+    print(arrayed)
+    chunked = []
+    base = 0
+    while base < len(arrayed) - height + 1:
+        grouped = np.array([], dtype=int)
+        for i in range(height):
+            grouped = np.append(grouped, arrayed[base + i])
+        chunked.append(np.copy(grouped))
+        base += height
+
+    return np.array(chunked)
