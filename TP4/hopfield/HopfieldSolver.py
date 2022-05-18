1f3e6ab786528a809c8e1fc0bd40a3bbf972dd56
JuanOriana <joriana@itba.edu.ar>
2022-05-18 09:44:16 -0300

    HopfieldSolver.py added

---
+++ TP4/hopfield/HopfieldSolver.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class HopfieldSolver:
+
+    def solve(self, query: np.ndarray, inputs: np.ndarray):
+        # Estimating weights
+        count, dimension = inputs.shape
+        K = np.zeros((count, dimension))
+        for i in range(len(inputs)):
+            K[i, :] = inputs[i]
+        weights = (1 / dimension) * (np.matmul(K.T, K))
+        np.fill_diagonal(weights, 0)
+
+        t = 0
+        S = query
+        old_S = None
+
+        while not np.array_equal(S, old_S):
+            old_S = S
+            S = np.sign(np.dot(weights, S))
+            t += 1
+
+        return S, t
+
+
+1
+
+if __name__ == "__main__":
+    solver = HopfieldSolver()
+    inputs = np.array([[1, 1, -1, -1], [-1, -1, 1, 1]])
+    solver.solve(np.array([1, -1, -1, -1]), inputs)
