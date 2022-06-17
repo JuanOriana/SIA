7a408dda61e5e2afceb8a40a0eb70732058a2398
JuanOriana <joriana@itba.edu.ar>
2022-06-17 15:42:16 -0300

    main

--- TP5/main.py
+++ TP5/main.py
@@ -5,7 +5,7 @@ import numpy as np
 from TP5.data_structs.Layer import Layer, NeuralNetwork
 from TP5.perceptron_funcs.activations import step_activation, linear_activation, sigmoid_classic_activation, \
     sigmoid_classic_activation_derivative, sigmoid_tanh_activation, sigmoid_tanh_activation_derivative
-from TP5.resources.fonts import get_parsed_fonts
+from TP5.resources.fonts import get_parsed_fonts, print_letter
 
 
 def main():
@@ -13,14 +13,14 @@ def main():
 
 
     layer1 = Layer(5, 35)
-    layer2 = Layer(2,5)
+    layer2 = Layer(2, 5)
     layer3 = Layer(5,2)
     layer4 = Layer(35,5)
 
     inputs = get_parsed_fonts()
 
     # Combine the layers to create a neural network
-    neural_network = NeuralNetwork([layer1,layer2,layer3,layer4],sigmoid_tanh_activation,sigmoid_tanh_activation_derivative,0.1)
+    neural_network = NeuralNetwork([layer1,layer2,layer3,layer4],sigmoid_classic_activation,sigmoid_classic_activation_derivative,0.1)
 
     "Stage 1) Random starting synaptic weights: "
     #neural_network.print_weights()
@@ -32,7 +32,7 @@ def main():
     # Do it 60,000 times and make small adjustments each time.
     start_time = time.time()
     print("training...")
-    neural_network.train(inputs, inputs, 10)
+    neural_network.train(inputs, inputs, 30)
     end_time = time.time()
     print(end_time - start_time)
 
@@ -48,8 +48,12 @@ def main():
 
     # Test the neural network with a new situation.
     "Stage 3) Considering a new situation [1, 1, 0] -> ?: "
-    outputs = neural_network.activate(inputs[0])
-    print (outputs[-1])
+    for i in range(len(inputs)):
+        print("EXPECTED LETTER")
+        print_letter(inputs[i])
+        print("FINAL LETTER")
+        print_letter(neural_network.activate(inputs[i])[-1])
+        print("/////////////////////////////////")
     print(neural_network.eval_error(inputs,inputs))
     # print(neural_network.eval_error(training_set_inputs,training_set_inputs))
 
